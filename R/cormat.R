bab01876f0b2d535affe55d1ea550a4ac1a43a32
freuerde <freuer_dennis@hotmail.de>
2022-03-09 16:10:10 +0100

    Update cormat.R

--- R/cormat.R
+++ R/cormat.R
@@ -14,31 +14,40 @@
 #' @importFrom stats cor as.dist hclust
 #' @export
 #'
-cormat <- function(df, print_cors=FALSE, rnd=NULL) {
-  if(!data.table::is.data.table(df)) { df <- data.table::data.table(df)}
+cormat <- function(df, print_cors=FALSE, rnd=NULL, method = c("pearson", "kendall", "spearman")) {
+  library(data.table)
+  if(!is.data.table(df)) { df <- data.table(df)}
+  # divide the data into numerical and categorical variables:
   sa <- sapply(df, class)
-  ind <- which(sa %in% c("numeric","integer"))
-
-  dnum <- df[, ind, with=FALSE]
-  dcat <- df[, -ind, with=FALSE]
-
-  cors <- cor(dnum, use="pairwise.complete.obs")
-
+  ind <- which(sa %in% "numeric")
+  
+  dnum <- df[, ind, with=FALSE] # all numerical variables
+  dcat <- df[, -ind, with=FALSE] # all categorical variables
+  
+  ### correlations:
+  cors <- cor(dnum, use="pairwise.complete.obs", method=method[1])
+  
+  # cluster correlations:
   dd <- as.dist((1-cors)/2)
   hc <- hclust(dd)
   cors <- cors[hc$order, hc$order]
-
-  cors[upper.tri(cors, diag=TRUE)] <- NA
-  cors <- reshape::melt(cors, na.rm=TRUE)
-
-  g <- ggplot(data=cors, aes(x=Var1, y=Var2, fill=value)) +
-    geom_tile()
-
+  
+  cors[upper.tri(cors, diag=TRUE)] <- NA # delete the upper triangle
+  cors <- reshape2::melt(cors, na.rm=TRUE)
+  
+  g <- ggplot(data=cors, aes(x=Var1, y=Var2, fill=value)) + geom_tile() + 
+    labs(x = NULL, y = NULL) #+ scale_fill_gradient("cor",limits = c(-1,1))
+  
+  
   cors <- cors[order(abs(cors$value), decreasing=TRUE),]
-  names(cors)[3] <- "Pearson_Cor"
-
+  names(cors)[3] <- "Pearson_Cor" 
+  
   if(!is.null(rnd)) {cors$Pearson_Cor <- round(cors$Pearson_Cor, rnd)}
+  names(cors)[3] <- paste0(method[1],"_cor")
   if(print_cors) {print(cors)}
-
+  
+  ### chi-square tests: (WAS IST MIT ALPHAKUMMULIERUNG???)
+  
+  
   return(list(plot=g,decreasing_correlations=cors))
 }
