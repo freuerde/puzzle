0ad736b5231c74d5aca2997dffa906256e6be8dc
freuerde <freuer_dennis@hotmail.de>
2022-03-10 09:46:41 +0100

    Update opendir.R

--- R/opendir.R
+++ R/opendir.R
@@ -1,6 +1,7 @@
 #' File access direclty from R
 #'
-#' Short function for opening a file directory within any operating system (Windows, MAC OS, etc.) from R directly.
+#' Short function for opening either a file directory within any operating system (Windows, MAC OS, etc.) 
+#' or a website (only working with the complete url) from R directly.
 #' @param dir Path, that should be opened with the current working directory as default.
 #' @author Dennis Freuer
 #' @export
