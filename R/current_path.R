964d35f57bee2d08a675b50107b8b3e30814b050
freuerde <freuer_dennis@hotmail.de>
2022-03-10 13:09:04 +0100

    Update current_path.R

--- R/current_path.R
+++ R/current_path.R
@@ -1,15 +1,10 @@
-#' Returns the current path
+#' Returns the path to folder of the current R-script
 #'
 #' Returns the current path to the folder, where the current R-script is stored.
-#' @param names Character vector that contains german umlaute.
-#' @param and Character vector.If not NULL the vector "and"
-#' (default: c("-"," ")) will be replaced by the element "by"
-#' @param by Character element. Set to NULL, if no replacement is desired.
 #' @return Returns the current path as a string.
 #' @author Dennis Freuer
 #' @export
 #'
 current_path <- function(){ return(dirname(rstudioapi::getSourceEditorContext()$path)) }
 
-#' @describeIn crnt_path() and current_path() are two identical functions, but with different notation
 crnt_path <- function(){ return(dirname(rstudioapi::getSourceEditorContext()$path)) }
