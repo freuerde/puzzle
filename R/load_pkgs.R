7f41e417c501e7e5b968a0c9b7d2691dbaf8bb9c
freuerde <freuer_dennis@hotmail.de>
2022-03-10 10:08:09 +0100

    Create load_pkgs.R

---
+++ R/load_pkgs.R
@@ -0,0 +1,16 @@
+#' Loads and installs multiple packages at once
+#'
+#' Loads and installs (if necessary) several packages at once.
+#' @param pkgs  vector of package names as character strings.
+#' @param install_new_pgs=TRUE Should unavailable packages be installed
+#' automatically using the install.packages() function? Default is TRUE. 
+#' @author Dennis Freuer
+#' @export
+#'
+load_pkgs <- function(pkgs, install_new_pgs=TRUE){
+  if(install_new_pgs){
+    install.packages(setdiff(pkgs, rownames(installed.packages())))
+  }
+  lapply(pkgs, library, character.only = TRUE)
+}
+
