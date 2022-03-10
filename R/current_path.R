3ef675e122fa0ac7c3d1f38ec9f2f98fb0e7a437
freuerde <freuer_dennis@hotmail.de>
2022-03-10 22:35:19 +0100

    Update current_path.R

--- R/current_path.R
+++ R/current_path.R
@@ -8,5 +8,4 @@
 current_path <- function(){ return(dirname(rstudioapi::getSourceEditorContext()$path)) }
 
 #' @describeIn current_path Shorter notation.
-
 crnt_path <- function(){ return(dirname(rstudioapi::getSourceEditorContext()$path)) }
